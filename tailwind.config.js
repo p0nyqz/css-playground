49879774f79c0ff1d19f2afe2c02de170aac3b35
p0nyqz <ponyquiz@gmail.com>
2022-08-13 17:53:43 +0200

    tailwind: custom font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
